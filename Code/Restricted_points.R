52655e13e9a014e505dbe285a8bbcd443fe13f79
Caroline Tait <cjtaituk@yahoo.co.uk>
2020-04-03 11:18:25 +0100

    restricted points added

---
+++ Code/Restricted_points.R
@@ -0,0 +1,87 @@
+####################################
+# TFL CID - restricted points      #
+# Created 03/04/2020               #
+####################################
+
+# install packages
+library(tidyverse)
+library(CycleInfraLnd)
+library(sf)
+library(tmap)
+library(mapview)
+library(leafsync)
+library(summarytools)
+library(forcats)
+library(geojsonsf)
+
+# download CID data using the Cycle Infra Lnd package
+restricted_points = get_cid_points(type = "restricted_point")
+class(restricted_points)
+str(restricted_points)
+
+# convert certain columns to factors
+f_variables = c("RST_STEPS", "RST_LIFT")
+
+# convert columns to factors (BOROUGH needs doing separately as it has some NAs in, CLT_ACCESS not converted as 721 different values)
+f_restricted_points = restricted_points %>%
+  mutate_at(f_variables, as.factor)
+f_restricted_points$BOROUGH = factor(f_restricted_points$BOROUGH, exclude = NULL)
+
+glimpse(f_restricted_points) # check converted ok
+levels(f_restricted_points$BOROUGH) # have 27 and no NA value
+
+# create new df without geommetry that enables faster analysis of data
+non_geom_f_restricted_points = st_drop_geometry(f_restricted_points)
+str(non_geom_f_restricted_points)
+
+# create summary of df
+view(dfSummary(non_geom_f_restricted_points))
+
+# Read in London Boroughs to add to map and code so can be joined to CID data
+boroughs <- st_read("./map_data/London_Borough_Excluding_MHW.shp")
+boroughs = rename(boroughs, BOROUGH = NAME)
+boroughs$BOROUGH = fct_recode(boroughs$BOROUGH, "Kensington & Chelsea" = "Kensington and Chelsea", 
+                              "Barking & Dagenham" = "Barking and Dagenham",
+                              "Hammersmith & Fulham" = "Hammersmith and Fulham")
+
+
+#a) map borough level data of number of restricted points
+count_restricted_pointsBYborough = non_geom_f_restricted_points %>%
+  group_by(BOROUGH) %>%
+  summarise(count = n())
+
+# join numbers to geometry
+n_restricted_pointsBYborough = left_join(boroughs, count_restricted_pointsBYborough)
+
+# plot counts of restricted points by Borough
+qtm(n_restricted_pointsBYborough, "count")
+  
+Restricted_points_borough_map = tm_shape(n_restricted_pointsBYborough) +
+  tm_polygons("count", style = "fixed", palette = "Greens",
+              breaks = c(1, 5, 10, 15, 20, 50),
+              textNA = "Zero restricted points",
+              colorNA = "grey") +
+  tm_layout(legend.title.size = 1,
+            legend.text.size = 0.7,
+            legend.position = c("left","bottom"),
+            legend.bg.alpha = 1)
+  
+tmap_save(Restricted_points_borough_map, filename = "./Maps/Count_restricted_points_borough_map.png")
+
+# Overall map of restricted points
+map_restricted_points = mapview(f_restricted_points$geometry, color = "blue", cex = 0.5)
+mapshot(map_restricted_points, file = paste0(getwd(),"/Maps/","All_restricted_points_map.png"),
+        remove_controls = c("zoomControl", "layersControl", "homeButton"))
+
+# Mapping restricted points to TFL cycling infrastructure
+# Obtain TFL Cycle route data
+urlCR = "https://cycling.data.tfl.gov.uk/CycleRoutes/CycleRoutes.json"
+TFL_CR = geojson_sf(urlCR)
+
+open_TFL_CR = TFL_CR %>%
+  filter(Status == "Open") # limit to infrastructure that is open
+
+
+# Create overlapping map of Restriced points and TFL cycle routes
+mapview(f_restricted_points$geometry, color = "red", cex = 1) + mapview(open_TFL_CR$geometry, lwd = 3)
+
---
+++ Code/Signage.R
@@ -0,0 +1,102 @@
+####################################
+# TFL CID - signage                 #
+# Created 03/04/2020               #
+####################################
+
+# install packages
+library(tidyverse)
+library(CycleInfraLnd)
+library(sf)
+library(tmap)
+library(mapview)
+library(leafsync)
+library(summarytools)
+library(forcats)
+library(geojsonsf)
+
+# download CID data using the Cycle Infra Lnd package
+
+signage = get_cid_points(type = "signage")
+class(signage)
+str(signage)
+
+GOT HERE
+
+
+# convert certain columns to factors
+f_variables = c("SIG_HEAD", "SIG_SEPARA", "SIG_EARLY", "SIG_TWOSTG", "SIG_GATE")
+
+# convert columns to factors (BOROUGH needs doing separately as it has some NAs in, CLT_ACCESS not converted as 721 different values)
+f_signal = signal %>%
+  mutate_at(f_variables, as.factor)
+f_signal$BOROUGH = factor(signal$BOROUGH, exclude = NULL)
+
+glimpse(f_signal) # check converted ok
+levels(f_signal$BOROUGH) # only have 23 and no NA value
+
+# create new df without geommetry that enables faster analysis of data
+non_geom_f_signal = st_drop_geometry(f_signal)
+str(non_geom_f_signal)
+non_geom_f_signal %>%
+  count(SIG_HEAD)  
+
+# create summary of df
+view(dfSummary(non_geom_f_signal))
+
+# Read in London Boroughs to add to map and code so can be joined to CID data
+boroughs <- st_read("./map_data/London_Borough_Excluding_MHW.shp")
+boroughs = rename(boroughs, BOROUGH = NAME)
+boroughs$BOROUGH = fct_recode(boroughs$BOROUGH, "Kensington & Chelsea" = "Kensington and Chelsea", 
+                              "Barking & Dagenham" = "Barking and Dagenham",
+                              "Hammersmith & Fulham" = "Hammersmith and Fulham")
+
+
+#a) map borough level data of count
+count_signalBYborough = non_geom_f_signal %>%
+  group_by(BOROUGH) %>%
+  summarise(count = n())
+
+# delete 'NA' row
+# count_signalBYborough = count_signalBYborough[-c(34),]  NOT REQUIRED AS NO NA ROW
+
+# join numbers to geometry
+n_signalBYborough = left_join(boroughs, count_signalBYborough)
+
+# plot counts of signals by Borough
+qtm(n_signalBYborough, "count")
+  
+Count_signal_borough_map = tm_shape(n_signalBYborough) +
+  tm_polygons("count", style = "fixed", palette = "Greens", 
+              breaks = c(1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100),
+              textNA = "Zero signals",
+              colorNA = "grey") +
+  tm_text("count") +
+  tm_layout(legend.title.size = 1,
+            legend.text.size = 0.7,
+            legend.position = c("right","bottom"),
+            legend.bg.alpha = 1)
+  
+tmap_save(Count_signal_borough_map, filename = "./Maps/Count_signal_borough_map.png")
+
+# Overall map of signals
+map_signals
+
+map_signals = mapview(f_signal$geometry, color = "blue", cex = 1)
+mapshot(map_signals, file = paste0(getwd(),"/Maps/","All_signals_map.png"),
+        remove_controls = c("zoomControl", "layersControl", "homeButton"))
+
+# Mapping segregated cycling infrastructure
+# Obtain TFL Cycle route data
+urlCR = "https://cycling.data.tfl.gov.uk/CycleRoutes/CycleRoutes.json"
+TFL_CR = geojson_sf(urlCR)
+
+open_TFL_CR = TFL_CR %>%
+  filter(Status == "Open") # limit to infrastructure that is open
+map_open_TFL_CR = mapview(open_TFL_CR$geometry) # map it
+
+
+# Create synchronised map of CID signals v open TFL cycle routes 
+sync(map_signals, map_open_TFL_CR, no.initial.sync = FALSE) 
+
+# Create overlapping map of Signal and TFL cycle routes
+mapview(f_signal$geometry, color = "blue", cex = 1) + mapview(open_TFL_CR$geometry)
